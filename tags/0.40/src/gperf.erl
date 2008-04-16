c2a042bbffd77b1e985f9d39c52ff130a316a157
mats.cronqvist <mats.cronqvist@5f96f306-4121-0410-aeb0-fb9935354dc2>
2008-04-16 16:27:19 +0000

    release 0.40, right before the big /proc rewrite

---
+++ tags/0.40/src/gperf.erl
@@ -0,0 +1,12 @@
+-module(gperf).
+-export([ni/1,start/1,start/0,log/2]).
+
+ni(Args) -> self() ! {args, Args}, gperfGtk:init().
+
+start() -> start(['']).
+
+start(Args) -> gperfGtk:start() ! {args,Args}.
+
+log(ProcInfo,Term) when not is_list(Term) -> log(ProcInfo,[Term]);
+log(ProcInfo,List) ->
+    error_logger:info_report([{in,CF}||{current_function,CF}<-ProcInfo]++List).
